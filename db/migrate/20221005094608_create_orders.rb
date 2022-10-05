570fe31fa1b072784416d28d91cc9b1713419634
bonveeee <bonochieng@gmail.com>
2022-10-05 13:10:26 +0300

    [add]model complete setup

---
+++ db/migrate/20221005094608_create_orders.rb
@@ -0,0 +1,13 @@
+class CreateOrders < ActiveRecord::Migration[7.0]
+  def change
+    create_table :orders do |t|
+      t.float :amount
+      t.integer :status , default: 0
+      t.string :date
+      t.string :card_data
+      t.belongs_to :user
+
+      t.timestamps
+    end
+  end
+end
